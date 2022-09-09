f62f7c18c424efc417fe7ae5ed1dc290c884790a
Alex-chulu <chulualexandar@gmail.com>
2022-09-09 11:39:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int y;
+for (y = 0; y < 10; y++)
+{
+printf("%y", y);
+}
+
+printf('\n');
+
+return (0);
+}

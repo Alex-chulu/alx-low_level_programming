cf6c63872270658b51e328deea04f7337ecc75cd
Alex-chulu <chulualexandar@gmail.com>
2022-09-08 19:47:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * Description: a amin function that prints out a string.
  * Return: 0if success.
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

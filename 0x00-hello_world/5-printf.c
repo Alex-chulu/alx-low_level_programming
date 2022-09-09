00628663940b6fb8099837ca1808b8057d0256b1
Alex-chulu <chulualexandar@gmail.com>
2022-09-08 20:09:11 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * Description: main function that prints to the
- * stdout.
+ *main - print the string in thr printf function
+ *Description: main function that prints to the stdout.
+ *This program prints "with proper grammar,
+ *but the outcome is a piece of art,"
  *Return: 0if success.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar,but the outcome is a piece of art, \n");
 return (0);
 }

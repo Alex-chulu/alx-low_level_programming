ba3d8ffccdca7afdb2e60f1bdb853fae8eb7da37
Alex-chulu <chulualexandar@gmail.com>
2022-09-14 01:40:01 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int num, multi, produc;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchaar('0');
+
+		for (multi = 1; multi <= 9; mutlti++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			produc = num * multi;
+			if (produc <= 9)
+			{
+				_putchar(' ');
+			else
+				_putchar((produc / 10) + '0');
+
+			_putchar((produc % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}

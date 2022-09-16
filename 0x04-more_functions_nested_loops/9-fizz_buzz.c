68f02e344cc1197abddc9bf669f37169a566fc64
Alex-chulu <chulualexandar@gmail.com>
2022-09-16 13:45:05 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,35 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * Return: Always 0 for success
- */
+* main - prints the numbers from 1 to 100, followed by a new line
+* Return: Always 0 for success
+*/
 int main(void)
 {
-	int y;
-	char fizz[] = "Fizz";
-	char buzz[] = "Buzz";
-	char fizzbuzz[] = "FizzBuzz";
 
-	for (y = 1; y <= 100; y++)
-	{
-		if (y == 100)
-		{
-			printf("%s", buzz);
-		}
-		else if ((y % 3 == 0) && (y % 5 == 0))
-		{
-			printf("%s", fizzbuzz);
-		}
-		else if (y % 3 == 0)
-		{
-			printf("%s", fizz);
-		}
-		else if (y  % 5 == 0)
-		{
-			printf("%s", buzz);
-		}
-		else
-		{
-			printf("%d", y);
-		}
-	}
-	printf("\n");
-	return (0);
+int y;
+
+for (y = 1; y <= 100; y++)
+{
+if (y % 3 == 0 && y % 5 != 0)
+{
+printf("Fizz");
+}
+else if (y % 5 == 0 && y % 3 != 0)
+{
+printf("Buzz");
+}
+else if (y % 3 == 0 && y % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (y == 1)
+{
+printf("%d", y);
+}
+else
+{
+printf("%d", y);
+}
 }

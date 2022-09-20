8bff08d7b13b0326951fd26bd5f10e8c3a5cc6e1
Alex-chulu <chulualexandar@gmail.com>
2022-09-19 17:49:54 -0700

    Arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: string
+ * Return 0
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (*(str + x) != '\0')
+	{
+		if (x % 2 == 0)
+		{
+			putchar(*(str + x));
+		}
+		x++
+	}
+	putchar(10);
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: array
+ * @n: number of elements of the array to be printed
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int chulu;
+
+	for (chulu = 0; chulu < n; chulu++)
+	{
+		if (chulu != n - 1)
+		{
+			printf("%d", a[chulu]);
+		}
+		else
+		{
+			printf("%d", a[chulu]);
+		}
+	}
+	putchar(10);
+}

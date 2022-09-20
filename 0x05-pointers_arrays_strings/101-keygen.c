b47d1f92858d78cc7dddc993d9d46c2369f2dfe2
Alex-chulu <chulualexandar@gmail.com>
2022-09-19 21:25:28 -0700

    generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y;
+	int i;
+
+	srand(time(0));
+
+	while (i <= 2788)
+	{
+		y = rand() % 128;
+		i += y;
+		putchar(y);
+	}
+	putchar(277 - i);
+
+	return (0);
+}

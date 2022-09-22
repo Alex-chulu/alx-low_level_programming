ac2d43cf296315ec874e2f83fc54e1c1385cd8ec
Alex-chulu <chulualexandar@gmail.com>
2022-09-22 16:36:31 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *string_toupper - a function that changes all lowercase
+ *letters of a string to uppercase.
+ *@str: string parameter
+ *Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int alx = 0;
+
+	while (str[alx++])
+	{
+		if (str[alx] >= 'a' && str[alx] <= 'z')
+		{
+			str[alx] -= 32
+		}
+	}
+	return (str);
+}

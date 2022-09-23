8423642f91410b95dd7db3e93915292216dc5aa3
Alex-chulu <chulualexandar@gmail.com>
2022-09-22 17:12:05 -0700

    a function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet -  encodes a string into 1337
+ * @str: string
+ * Return: string
+ */
+
+char *leet(char *str)
+{
+	int alx = 0;
+	int chu;
+	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++alx])
+	{
+		for (chu = 0; chu <= 7; chu++)
+		{
+			if (str[alx] == leet[chu] ||
+			str[alx] - 32 == leet[chu])
+			{
+				str[alx] = chu + '0';
+			}
+		}
+	}
+	return (str);
+}

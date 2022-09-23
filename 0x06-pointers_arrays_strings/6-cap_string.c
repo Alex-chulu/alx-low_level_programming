1323b3cd7a8ef9ab3591268416f11b3e335948ee
Alex-chulu <chulualexandar@gmail.com>
2022-09-22 19:58:23 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,30 @@
 
 char *cap_string(char *str)
 {
-	int x = 0, y;
-	int alx = 13;
-	char space[] = {32, '\t', '\n', 44, ';',
-	46, '!', '?', '"', '(', ')', '{', '}'};
+	int alx = 0;
 
-	while (str[x])
+	while (str[++alx])
 	{
-		y = 0;
-
-		while (y < alx)
+		while (!(str[alx] >= 'a' && str[alx] <= 'z'))
+				{
+					alx++;
+				}
+		if (str[alx - 1] == ' ' ||
+				str[alx - 1] == '\t' ||
+				str[alx - 1] == '\n' ||
+				str[alx - 1] == ',' ||
+				str[alx - 1] == ';' ||
+				str[alx - 1] == '.' ||
+				str[alx - 1] == '!' ||
+				str[alx - 1] == '?' ||
+				str[alx - 1] == '"' ||
+				str[alx - 1] == '(' ||
+				str[alx - 1] == ')' ||
+				str[alx - 1] == '{' ||
+				str[alx - 1] == '}')
 		{
-			if ((x == 0 || str[x - 1] == space[y] &&
-			str[x] >= 97 && str[x] <= 122))
-			{
-				str[x] -= 32;
-			}
-			y++;
+			str[alx] -= 32;
 		}
-		x++;
 	}
 	return (str);
 }

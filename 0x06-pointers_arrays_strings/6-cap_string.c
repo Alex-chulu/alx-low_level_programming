a5be4479c06333752a3594c26d40ca9ecb61aff8
Alex-chulu <chulualexandar@gmail.com>
2022-09-23 19:15:09 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,34 +2,34 @@
 #include <stdio.h>
 
 /**
- * cap_string - a function that capitalizes all words of a string
- * @str: string
- * Return: string
- */
+* cap_string - a function that capitalizes all words of a string
+* @str: string
+* Return: string
+*/
 
 char *cap_string(char *str)
 {
-	int alx = 0;
+int alx = 0;
 
-	while (str[++alx])
-	{
-		while (!(str[alx] >= 'a' && str[alx] <= 'z'))
-			alx++;
+while (str[++alx])
+{
+while (!(str[alx] >= 'a' && str[alx] <= 'z'))
+alx++;
 
-		if (str[alx - 1] == ' ' ||
-				str[alx - 1] == '\t' ||
-				str[alx - 1] == '\n' ||
-				str[alx - 1] == ',' ||
-				str[alx - 1] == ';' ||
-				str[alx - 1] == '.' ||
-				str[alx - 1] == '!' ||
-				str[alx - 1] == '?' ||
-				str[alx - 1] == '"' ||
-				str[alx - 1] == '(' ||
-				str[alx - 1] == ')' ||
-				str[alx - 1] == '{' ||
-				str[alx - 1] == '}')
-			str[alx] -= 32;
-	}
-	return (str);
+if (str[alx - 1] == ' ' ||
+str[alx - 1] == '\t' ||
+str[alx - 1] == '\n' ||
+str[alx - 1] == ',' ||
+str[alx - 1] == ';' ||
+str[alx - 1] == '.' ||
+str[alx - 1] == '!' ||
+str[alx - 1] == '?' ||
+str[alx - 1] == '"' ||
+str[alx - 1] == '(' ||
+str[alx - 1] == ')' ||
+str[alx - 1] == '{' ||
+str[alx - 1] == '}')
+str[alx] -= 32;
+}
+return (str);
 }

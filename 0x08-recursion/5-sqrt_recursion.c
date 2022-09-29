1fece64c6db3a22ee17839149697306ffd258b0e
Alex-chulu <chulualexandar@gmail.com>
2022-09-28 20:16:50 -0700

    task 5 - hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,3 +14,23 @@ int _sqrt_recursion(int n)
 	}
 	return (_sqrt(0, n));
 }
+
+/**
+ * _sqrt - square root of a number
+ * @n: test number
+ * @x: number to sqare
+ * Return: sqare root
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+	{
+		return (-1);
+	}
+	else if (n * n == x)
+	{
+		return (n);
+	}
+	return (_sqrt(n + 1, x));
+}

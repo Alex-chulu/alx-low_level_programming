4c31814cbb69c89d0a2b916880ab5e3a17942e66
Alex-chulu <chulualexandar@gmail.com>
2022-09-29 20:01:27 -0700

    task 8- hot fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == *s1)
 	{
-		return (*s1 != '\0' && wildcmp(s1 +1, s2 + 1));
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s2 == '*')
 	{

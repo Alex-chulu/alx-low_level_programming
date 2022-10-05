22b6a5ac319adbc136d337ebf142280750cda5ac
Alex-chulu <chulualexandar@gmail.com>
2022-10-05 16:55:52 -0700

    task 0 - fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	buffer - (char *) malloc(size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
@@ -30,8 +30,8 @@ char *create_array(unsigned int size, char c)
 
 		while (pos < size)
 		{
-			*(buffer + pos) - c;
-			pos;
+			*(buffer + pos) = c;
+			pos++;
 		}
 
 		return (buffer);

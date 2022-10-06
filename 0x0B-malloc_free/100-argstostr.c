18b9e1155c2b0ecc6c6fa4fc69e18a2b3986ba9b
Alex-chulu <chulualexandar@gmail.com>
2022-10-05 19:51:25 -0700

    a function that concatenates all the arguments of the program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * argstostr - a function that concatenates all the arguments of your program
+ * @ac: grid width
+ * @av: grid height
+ * Return: pointer to a new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int num_count = 0, a = 0, b = 0, c = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	while (a < ac)
+	{
+		b = 0;
+		while (av[a][b] != '\0')
+		{
+			num_count++;
+			b++;
+		}
+		a++;
+	}
+	num_count = num_count + ac + 1;
+	str = malloc(sizeof(char) * num_count);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\0';
+		c++;
+	}
+	return (str);
+}

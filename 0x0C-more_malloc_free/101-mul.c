5eae6e618928cab8ea9573348cc1dc1f43b46d88
Alex-chulu <chulualexandar@gmail.com>
2022-10-07 18:50:01 -0700

    a program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - a program that multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long multi;
+	int x, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (x = 1; x < argc; x++)
+	{
+		for (j = 0; argv[x][j] != '\0'; j++)
+		{
+			if (argv[x][j] > 57 || argv[x][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	multi =  atoi(argv[1]) * atoi(argv[2]);
+	printf("%lu\n", multi);
+
+	return (0);
+}

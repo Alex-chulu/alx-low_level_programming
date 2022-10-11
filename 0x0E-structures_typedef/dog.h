b14a3d10d62bf4e6904abb899384901fda17902f
Alex-chulu <chulualexandar@gmail.com>
2022-10-10 18:04:06 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,10 @@ struct dog
 	float age;
 };
 
-typedef struct dog dog_x;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_x *new_dog(char *name, float age, char *owner);
-void free_dog(dog_x *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /*DOG_H*/

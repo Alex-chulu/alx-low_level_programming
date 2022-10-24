a5a78bda646007155a521b91c65fcb9fe8c151e8
Alex-chulu <chulualexandar@gmail.com>
2022-10-23 22:29:14 -0700

    task 3 fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dupp;
+	char *dup;
 	int n;
 	list_t *new_n, *last_n;
 
@@ -18,21 +18,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_n == NULL)
 		return (NULL);
 
-	dupp = strdup(str);
+	dup = strdup(str);
 	if (str == NULL)
 	{
 		free(new_n);
 		return (NULL);
 	}
+
 	for (n = 0; str[n];)
 		n++;
 
-	new_n->str = dupp;
+	new_n->str = dup;
 	new_n->n = n;
 	new_n->next = NULL;
 
 	if (*head == NULL)
 		*head = new_n;
+
 	else
 	{
 		last_n = *head;

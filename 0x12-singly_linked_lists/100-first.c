d840296fa1b9a49e0a4ed884b008225bc34d3d21
Alex-chulu <chulualexandar@gmail.com>
2022-10-23 21:50:21 -0700

    task 5 fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
-
 /**
- * hare - prints string before main executes
+ * tortoise - prints string before main executes
  */
 
-void hare(void)
+void __attribute__((constructor)) tortoise()
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }

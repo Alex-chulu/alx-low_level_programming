253151c17be9b1daaeaa59e841d2b89cb864d9a2
Alex-chulu <chulualexandar@gmail.com>
2022-10-26 16:26:03 -0700

    task 0 hot fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 0;
+	size_t node_list = 0;
 
 	while (h)
 	{
-		node++;
-		printf("%d\n", h->node);
+		node_list++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (node);
+	return (node_list);
 }

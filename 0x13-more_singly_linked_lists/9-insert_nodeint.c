025d1f974bd8dca0198d00bf5428ff7737a717a5
Alex-chulu <chulualexandar@gmail.com>
2022-10-29 02:51:38 -0700

    task 9 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node->next = dup->next;
-	copy->next = new_node;
+	dup->next = new_node;
 
 	return (new_node);
 }

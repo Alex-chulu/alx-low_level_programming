38108ee14f7d5a183e665e71768b3b53447a0a9a
Alex-chulu <chulualexandar@gmail.com>
2022-10-29 03:14:56 -0700

    task 10 fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (node = 0; node < (index - 1); node++)
 	{
 		if (dup->next == NULL)
-			return (NULL);
+			return (-1);
 
 		dup = dup->next;
 	}

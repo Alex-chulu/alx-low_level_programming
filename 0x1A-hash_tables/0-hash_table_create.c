8d4996e1fa8d4e9ccbd07f7213c5c82dbfaf72ba
Alex-chulu <chulualexandar@gmail.com>
2023-01-18 14:50:13 +0200

    task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hasht;
 	unsigned long int x;
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (hasht == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	hasht->size = size;
+	hasht->array = malloc(sizeof(hash_node_t *) * size);
+	if (hasht->array == NULL)
 		return (NULL);
 	for (x = 0; x < size; x++)
-		ht->array[x] = NULL;
+		hasht->array[x] = NULL;
 
-	return (ht);
+	return (hasht);
 }
